984ec78ec3363a0f023df58cebdea9cc481a5c0a
Jhon Varon <jhonvaronyate@hotmail.com>
2024-02-09 19:13:51 -0500

    Update SearchResults component and fix prop type
    The "SearchResultsProps" type has been simplified to accept any type for the 'searchedPosts' prop instead of a specific 'DocumentList' model. In addition, the SearchResults component has been refactored to handle different search results scenarios more effectively. A typo in the name of a prop has also been fixed.

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,20 +1,21 @@
 import Loader from "./Loader";
 import GridPostList from "./GridPostList";
-import {Models} from "appwrite";
 
 
-type SearchResultsProps = {
+export type SearchResultProps = {
   isSearchFetching: boolean;
-  searchedPosts:  Models.DocumentList<any>| undefined;
+  searchedPosts: any;
 };
 
-const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
-  console.log()
-  if (isSearchFetching) return <Loader />;
-  if (searchedPosts && searchedPosts?.documents.length > 0) {
-    return <GridPostList posts={searchedPosts?.documents} />;
+const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultProps) => {
+  if (isSearchFetching) {
+    return <Loader />;
+  } else if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  } else {
+    return (
+        <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+    );
   }
-
-  return <p className="text-light-4 mt-10 text-center w-full">No results found</p>;
 };
 export default SearchResults;
